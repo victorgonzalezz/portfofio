0a0a9c7501ac989d593c6d4588b70d70e433849e
Victor <vitorpcg@gmail.com>
2022-10-06 19:52:18 +0100

    working on loading error image

--- components/About.jsx
+++ components/About.jsx
@@ -27,7 +27,7 @@ const About = () => {
           
         </div>
         <div className="w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300">
-        <Image src="/../public/assets/keyboard.jpg" alt="/" width="520" height="400" />
+        <Image src="../public/assets/keyboard.jpg" alt="/" width="520" height="400" />
         </div>
       </div>
     </div>
