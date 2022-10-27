109df74acd21442739eb8cb4479b3ac9a3d10e20
Victor <vitorpcg@gmail.com>
2022-10-27 17:27:38 +0100

    updated

--- components/Main.jsx
+++ components/Main.jsx
@@ -12,15 +12,15 @@ const Main = () => {
       <div className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center">
         <div>
           <p className="uppercase text-sm tracking-widest text-gray-600">
-            LET´S BUILD SOMETHING TOGETHER
+            LET'S BUILD SOMETHING TOGETHER
           </p>
           <h1 className="py-4 text-gray-700">
             Hi, I'm <span className="text-[#5651e5]">Victor</span>
           </h1>
           <h1 className="py-2 text-gray-700">A Front-End Web Developer</h1>
           <p className="py-4 text-gray-600 max-w-[70%] m-auto">
-          I´m a Front-end web developer deeply envolve in building awesome digital experiences. 
-          Currently, I´m focused on building responsive front-end web applications.
+          I'm a Front-end web developer deeply involved in building awesome digital experiences. 
+          Currently, I'm focused on building responsive front-end web applications.
           </p>
           <div className="flex items-center justify-between max-w-[330px] m-auto py-4">
           <a
